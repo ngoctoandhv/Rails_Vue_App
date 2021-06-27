c4e37349a15b78fcc7f87f96b737f11568ffec65
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2021-06-27 23:21:41 +0700

    init project

---
+++ vuefont/src/components/Contacts.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="contacts">
+  <p>toan</p>
+  <div v-for="contact in contacts" :key="contact.id">
+   <p>{{contact.name}} is my {{contact.relationship}}</p>
+  </div>
+ </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'Contacts',
+  data () {
+    return {
+      contacts: [],
+      name: [],
+      relationship: [],
+      errors: []
+    }
+  },
+  created () {
+    axios.get('http://localhost:3000/contacts')
+      .then(response => {
+        this.contacts = response.data
+        console.log(response.data)
+      })
+      .catch(e => {
+        this.error.push(e)
+      })
+  }
+}
+</script>
---
+++ vuefont/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <h1>My Vue</h1>
+    <ul class="nav-link">
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/contacts">Contacts</router-link></li>
+      <li>About</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+nav {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+
+}
+
+a {
+  color: #fff;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+nav ul.nav-link {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+}
+</style>
